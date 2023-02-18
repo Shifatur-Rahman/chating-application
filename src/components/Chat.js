caf361c9e4a2daf91a2890c87765a4327a0b84b8
Shifatur-Rahman <rjnovel28@gmail.com>
2023-02-18 08:55:06 +0600

    chat message image add

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -23,8 +23,81 @@ const Chat = () => {
 
         <div className="chatArea">
           <div className="chatMsg" style={alignLeft}>
-            <p style={msg}>Hey there</p>
-            <p className="date">02-August-2023 </p>
+            <p style={msgReceive}>Hey there</p>
+            <p style={dateSend} className="date">
+              02-August-2023{" "}
+            </p>
+          </div>
+
+          <div className="chatMsg" style={alignRight}>
+            <p style={msgSend}>Hey there</p>
+            <p style={dateReceive} className="date">
+              02-August-2023{" "}
+            </p>
+          </div>
+
+          <div className="chatMsg" style={alignLeft}>
+            <p style={msgReceive}>Hey there</p>
+            <p style={dateSend} className="date">
+              02-August-2023{" "}
+            </p>
+          </div>
+
+          <div className="chatMsg" style={alignRight}>
+            <p style={msgSend}>Hey there</p>
+            <p style={dateReceive} className="date">
+              02-August-2023{" "}
+            </p>
+          </div>
+
+          <div className="chatMsg" style={alignRight}>
+            <p style={msgSend}>Hey there</p>
+            <p style={dateReceive} className="date">
+              02-August-2023{" "}
+            </p>
+          </div>
+
+          <div className="chatMsg" style={alignLeft}>
+            <p style={msgReceive}>Hey there</p>
+            <p style={dateSend} className="date">
+              02-August-2023{" "}
+            </p>
+          </div>
+
+          <div className="chatMsg" style={alignRight}>
+            <p style={msgSend}>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book. It has
+              survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <p style={dateReceive} className="date">
+              02-August-2023{" "}
+            </p>
+          </div>
+
+          <div className="chatMsg" style={alignLeft}>
+            <div style={msgReceive} className="chatImg">
+              <img src="assets/images/login.png" />
+            </div>
+            <p style={dateSend} className="date">
+              02-August-2023{" "}
+            </p>
+          </div>
+
+          <div className="chatMsg" style={alignRight}>
+            <div style={msgSend} className="chatImg">
+              <img src="assets/images/login.png" />
+            </div>
+            <p style={dateReceive} className="date">
+              02-August-2023{" "}
+            </p>
           </div>
         </div>
       </div>
@@ -32,8 +105,13 @@ const Chat = () => {
   );
 };
 
-let msg = {
+let msgReceive = {
   background: "#F1F1F1",
+};
+
+let msgSend = {
+  background: "#5F35F5",
+  color: "#fff",
 };
 
 let alignRight = {
@@ -41,5 +119,13 @@ let alignRight = {
 };
 let alignLeft = {
   justifyContent: "flex-start",
+};
+
+let dateSend = {
+  left: "-40px",
+};
+
+let dateReceive = {
+  right: "-40px",
 };
 export default Chat;
