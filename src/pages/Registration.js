3506e2f8455e2576c93bfe401d0e75058834fe30
Shifatur-Rahman <rjnovel28@gmail.com>
2023-11-20 00:24:50 +0600

    form add

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -95,7 +95,7 @@ const Registration = () => {
                 helperText={nameErr}
                 id="demo-helper-text-misaligned"
                 label="FullName"
-                style={{ width: "365px", marginTop: "40px" }}
+                style={{ width: "365px" }}
                 type="text"
                 onChange={(e) => setName(e.target.value)}
               />
@@ -105,7 +105,7 @@ const Registration = () => {
                 helperText={emailErr}
                 id="demo-helper-text-misaligned"
                 label="Email"
-                style={{ width: "365px", marginTop: "40px" }}
+                style={{ width: "365px", marginTop: "10px" }}
                 type="email"
                 onChange={(e) => setEmail(e.target.value)}
               />
@@ -121,7 +121,7 @@ const Registration = () => {
                 }
                 id="demo-helper-text-misaligned"
                 label="Password"
-                style={{ width: "365px", marginTop: "40px" }}
+                style={{ width: "365px", marginTop: "10px" }}
                 type="password"
                 onChange={(e) => setPassword(e.target.value)}
               />
@@ -138,7 +138,7 @@ const Registration = () => {
                 }
                 id="demo-helper-text-misaligned"
                 label="Confirm Password"
-                style={{ width: "365px", marginTop: "40px" }}
+                style={{ width: "365px", marginTop: "10px" }}
                 type="password"
                 onChange={(e) => setConfirmpassword(e.target.value)}
               />
